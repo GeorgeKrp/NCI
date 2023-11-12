7ce8ea6c6109aacfdd87c25ef9a150d8ad5d0672
GeorgeKrp <134440527+GeorgeKrp@users.noreply.github.com>
2023-11-12 20:31:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function playGame(userChoice) {
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+  let result;
+
+  if (userChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (
+    (userChoice === 'rock' && computerChoice === 'scissors') ||
+    (userChoice === 'paper' && computerChoice === 'rock') ||
+    (userChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = "You win!";
+  } else {
+    result = "You lose!";
+  }
+
+  document.getElementById('result').textContent = `Your choice: ${userChoice} | Computer's choice: ${computerChoice} | Result: ${result}`;
+}
